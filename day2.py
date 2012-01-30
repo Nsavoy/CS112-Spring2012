f2c24a92d077e7a5983af4f6995651033978d636
Nathan Savoy <nathanielsavoy@gmail.com>
2012-01-30 11:25:32 -0500

    I updated my README

---
+++ day2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+day2.py
+
+a first program
+"""
+
+#the following line prints hello nurse to screen
+print "hello nurse!"
+print "Isn't this a wonderful day?!"
+print "lots of math to come"
+print "1 + 3=",1+3
+print "2 * 8=",2*8
+print "that was uber exciting."
+

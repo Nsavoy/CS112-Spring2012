aea79dd7ac129eba18dbb0d445dc81bd65e31a46
Nathan Savoy <nathanielsavoy@gmail.com>
2012-02-05 16:19:12 -0500

    hw03 done

---
+++ prissybot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Prissy bot, a cruel chat bot.
+"""
+
+print "Enter your name: "
+name=raw_input()
+print "Hello there,",name
+n=raw_input()
+print "You mean,",n,", sir!" #problem here with a space between N and comma.
+raw_input()
+print "That's nice, idiot. Can you do simple math?"
+raw_input()
+print "Uh-huh. No, you can't. Watch an expert. Give me a number."
+n=raw_input()
+print n, ", great. Now another."
+m=raw_input()
+n=int(n)
+m=int(m)
+print "okay,",n,"+",m,"=",n+m,", right, dribble-brain?"
+raw_input()
+print "I've translated that untenable gibberish as 'show me another, sir' and I will do so immediately, so long as you provide me with two less awful numbers, you infantile polyp."
+l=raw_input()
+print "And?"
+O=raw_input()
+l=float(l)
+O=float(O)
+print "Wonderful. You're a genius, like a rock or a scab. Here we go:",l,"*",O,"=",l*O,", you blockhead. What do you have to say for yourself, twit?"
+raw_input()
+print "You remind me of something a cat throws up... Only, I think you may be fouler. Since you prod me further, here's one more show of skills your puny brain can only dream of. Remember your four numbers? They were,",n,", ",m,", ",l,", and",O,", correct?"
+raw_input()
+print "Shut up. Watch: "
+j=n-m
+j=float(j)
+k=j/l
+k=float(k)
+print n,"-",m,"=",j," and ",j,"/",l,"=",j/l,". Moreover, ",k,"*",O,"=",k*O,". Right?"
+raw_input()
+print "That was rhetorical. Leave."
